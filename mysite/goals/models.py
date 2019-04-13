5730d8b500d7f4b03e6d3ba1974cefb4a43f3e27
yuriprym <samuel.young.103@gmail.com>
2019-04-13 11:00:12 -0400

    try to make the application so then when i add more thing to it

--- mysite/goals/models.py
+++ mysite/goals/models.py
@@ -1,11 +1,21 @@
 from django.db import models
 
+import datetime 
+
+from django.utils import timezone 
+
 # Create your models here.
 
 class Goals(models.Model):
    question_text= models.CharField(max_length=400)
    pub_date = models.DateTimeField('date goal was set')
+   def  was_published_recently(self):
+      return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+   def __str__(self):
+      return self.question_text
 class Choices(models.Model):
   question = models.ForeignKey(Goals, on_delete=models.CASCADE)
   choice_bool = models.BooleanField(False)
+  def __str__(self):
+      return self.choice_bool
 
