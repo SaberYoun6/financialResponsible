37e422711a51f4c11de6b1bd5ce0cf7ece0402d3
yuriprym <samuel.young.103@gmail.com>
2019-04-13 12:28:00 -0400

    signing This as any update and other things

--- mysite/goals/views.py
+++ mysite/goals/views.py
@@ -3,6 +3,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
-def index(request):
-   return HttpResponse("Hello world!")
 # Create your views here.
+
+def details(request, question_id):
+    return HttpReponse('Your goal that you are going to make is %s.'questiond_id)
+
+def acheivementGoal(requst,question_id):
+   buget=
