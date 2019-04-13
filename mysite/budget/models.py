8543f966b7fcf0c6e21ead19be1da5f553942c70
yuriprym <samuel.young.103@gmail.com>
2019-04-13 17:15:03 -0400

    a

--- mysite/budget/models.py
+++ mysite/budget/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
+import goals
+
+import goals from models
+
 # Create your models here.
+
+
+class Budget(models.Model):
+   budget=model.FloatField(default=1000)
+   def budget_choice():
+   if budget == Choices.budget:
+         if budget 
+      
